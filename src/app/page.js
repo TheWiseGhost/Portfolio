91dc040c9df902198195a6994a18d8d52c28223a
Aditya Byju <byjuaditya@gmail.com>
2025-02-06 23:02:25 -0600

    Mobile blocker

--- src/app/page.js
+++ src/app/page.js
@@ -28,13 +28,23 @@ export default function Home() {
   }, []);
 
   return (
-    <div ref={container}>
-      <MainLanding />
-      <MainProjects />
-      <MainClubs />
-      <MaskText />
-      <MagnetRow />
-      <Footer />
-    </div>
+    <>
+      <div className="hidden md:block" ref={container}>
+        <MainLanding />
+        <MainProjects />
+        <MainClubs />
+        <MaskText />
+        <MagnetRow />
+        <Footer />
+      </div>
+      <div className="h-screen flex md:hidden flex-col space-y-4 items-center justify-center text-center text-lg p-4 bg-white">
+        <p className="text-black font-gs">
+          Learn more about Aditya Byju on desktop{" "}
+        </p>
+        <p className="text-gray-800 font-gs text-sm">
+          There's too much to fit on mobile!
+        </p>
+      </div>
+    </>
   );
 }
